cc7ac1fa50332b6a9b94cc41d2627890028f40ee
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2016-10-16 19:47:48 -0400

    split out ROOT and REPLICA USER/PASS

--- mysql-replication-start.sh
+++ mysql-replication-start.sh
@@ -16,28 +16,37 @@
 #   - All hosts must be able to receive mysql commands remotely from the node executing this script
 #
 
-usage() { echo "Usage: $0" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -u REPLICA_USER -p REPLICA_PASS -U ROOT_USER -P ROOT_PASS -d DB -m MASTER_HOST -s SLAVE_HOSTS_WITH_COLON" 1>&2; exit 1; }
 
 # default values
 DB=djangodb
 
-USER=root
-PASS=root
+ROOT_USER=root
+ROOT_PASS=root
+
+REPLICA_USER=$ROOT_USER
+REPLICA_PASS=$ROOT_PASS
 
 MASTER_HOST=192.168.0.201
 SLAVE_HOSTS=(192.168.0.202 192.168.0.203)
 
 # override through options
-while getopts ":d:u:p:m:s:" o; do
+while getopts ":d:u:p:U:P:m:s:" o; do
 	case "${o}" in
 		d)
 			DB=${OPTARG}
 			;;
 		u)
-			USER=${OPTARG}
+			REPLICA_USER=${OPTARG}
 			;;
 		p)
-			PASS=${OPTARG}
+			REPLICA_PASS=${OPTARG}
+			;;
+		U)
+			ROOT_USER=${OPTARG}
+			;;
+		P)
+			ROOT_PASS=${OPTARG}
 			;;
 		m)
 			MASTER_HOST=${OPTARG}
@@ -63,8 +72,8 @@ DUMP_FILE="/tmp/$DB-export-$(date +"%Y%m%d%H%M%S").sql"
 
 echo "MASTER: $MASTER_HOST"
 
-mysql -h $MASTER_HOST "-u$USER" "-p$PASS" $DB <<-EOSQL &
-	GRANT REPLICATION SLAVE ON *.* TO '$USER'@'%' IDENTIFIED BY '$PASS';
+mysql -h $MASTER_HOST -u $ROOT_USER --password=$ROOT_PASS $DB <<-EOSQL &
+	GRANT REPLICATION SLAVE ON *.* TO '$REPLICA_USER'@'%' IDENTIFIED BY '$REPLICA_PASS';
 	FLUSH PRIVILEGES;
 	FLUSH TABLES WITH READ LOCK;
 	DO SLEEP(3600);
@@ -75,11 +84,11 @@ sleep 3
 
 # Dump the database (to the client executing this script) while it is locked
 echo "  - Dumping database to $DUMP_FILE"
-mysqldump -h $MASTER_HOST "-u$USER" "-p$PASS" --opt $DB > $DUMP_FILE
+mysqldump -h $MASTER_HOST "-u$ROOT_USER" "-p$ROOT_PASS" --opt $DB > $DUMP_FILE
 echo "  - Dump complete."
 
 # Take note of the master log position at the time of dump
-MASTER_STATUS=$(mysql -h $MASTER_HOST "-u$USER" "-p$PASS" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
+MASTER_STATUS=$(mysql -h $MASTER_HOST "-u$ROOT_USER" "-p$ROOT_PASS" -ANe "SHOW MASTER STATUS;" | awk '{print $1 " " $2}')
 LOG_FILE=$(echo $MASTER_STATUS | cut -f1 -d ' ')
 LOG_POS=$(echo $MASTER_STATUS | cut -f2 -d ' ')
 echo "  - Current log file is $LOG_FILE and log position is $LOG_POS"
@@ -101,16 +110,16 @@ for SLAVE_HOST in "${SLAVE_HOSTS[@]}"
 do
 	echo "SLAVE: $SLAVE_HOST"
 	echo "  - Creating database copy"
-	mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" -e "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB;"
+	mysql -h $SLAVE_HOST "-u$ROOT_USER" "-p$ROOT_PASS" -e "DROP DATABASE IF EXISTS $DB; CREATE DATABASE $DB;"
 	scp $DUMP_FILE $SLAVE_HOST:$DUMP_FILE >/dev/null
-	mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" $DB < $DUMP_FILE
+	mysql -h $SLAVE_HOST "-u$ROOT_USER" "-p$ROOT_PASS" $DB < $DUMP_FILE
 
 	echo "  - Setting up slave replication"
-	mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" $DB <<-EOSQL &
+	mysql -h $SLAVE_HOST "-u$ROOT_USER" "-p$ROOT_PASS" $DB <<-EOSQL &
 		STOP SLAVE;
 		CHANGE MASTER TO MASTER_HOST='$MASTER_HOST',
-		MASTER_USER='$USER',
-		MASTER_PASSWORD='$USER',
+		MASTER_USER='$REPLICA_USER',
+		MASTER_PASSWORD='$REPLICA_PASS',
 		MASTER_LOG_FILE='$LOG_FILE',
 		MASTER_LOG_POS=$LOG_POS;
 		START SLAVE;
@@ -118,7 +127,7 @@ do
 	# Wait for slave to get started and have the correct status
 	sleep 2
 	# Check if replication status is OK
-	SLAVE_OK=$(mysql -h $SLAVE_HOST "-u$USER" "-p$PASS" -e "SHOW SLAVE STATUS\G;" | grep 'Waiting for master')
+	SLAVE_OK=$(mysql -h $SLAVE_HOST "-u$ROOT_USER" "-p$ROOT_PASS" -e "SHOW SLAVE STATUS\G;" | grep 'Waiting for master')
 	if [ -z "$SLAVE_OK" ]; then
 		echo "  - Error ! Wrong slave IO state."
 	else
